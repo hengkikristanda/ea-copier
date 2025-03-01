88ba2b8d190a6f9371e6dd954be36247ec57e406
hengkikristanda <hengki.kristanda@binus.ac.id.com>
2025-03-01 17:07:37 +0700

    db config prod

--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,28 @@ const { config } = require("dotenv");
 
 config(); // Load environment variables
 
+// SSH
+// const sequelize = new Sequelize(
+// 	process.env.DB_DATABASE,
+// 	process.env.DB_USER,
+// 	process.env.DB_PASSWORD,
+// 	{
+// 		host: "127.0.0.1",
+// 		port: 3307, // The local port of the manually opened SSH tunnel
+// 		dialect: "mysql",
+// 		dialectModule: require("mysql2"),
+// 		logging: false,
+// 	}
+// );
+
+// LOCAL
 const sequelize = new Sequelize(
 	process.env.DB_DATABASE,
 	process.env.DB_USER,
 	process.env.DB_PASSWORD,
 	{
-		host: "127.0.0.1",
-		port: 3307, // The local port of the manually opened SSH tunnel
+		host: process.env.DB_HOST,
 		dialect: "mysql",
-		dialectModule: require("mysql2"),
-		logging: false,
 	}
 );
 
